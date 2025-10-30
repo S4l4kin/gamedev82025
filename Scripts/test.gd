57b9d18466e33c3f9f221f4d72a26fb67185a451
Erik Åström <mr.erik.astrom@gmail.com>
2025-10-30 13:10:06 +0200

    Finalized _deck_shuffler, _draw_weighted_card, _deal_hand

--- Scripts/test.gd
+++ Scripts/test.gd
@@ -1,2 +1,10 @@
-func test():
-	print("This works?! nice.")
+extends Node
+
+@onready var card_manager : CardManager = $"../CardManager"
+
+func _ready():
+	call_deferred("make_card")
+	
+func make_card():
+	var card = card_manager.get_card_scene("test_card").instantiate()
+	add_child(card)
