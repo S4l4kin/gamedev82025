f9779d7d4a80218923585dc866ad836294f8d65a
unknown <starclay666@gmail.com>
2026-02-26 13:23:38 +0200

    I thought I pushed this but maybe not: spawner spawn sound added

--- Scripts/Actor_scripts/Unit.gd
+++ Scripts/Actor_scripts/Unit.gd
@@ -18,11 +18,10 @@ func attack(enemy:Actor) -> bool:
 	enemy.on_pre_defend(self)
 
 	#Deal damage to both attacker and defender
-	AudioManager.play_3d_sfx_for_all("unit_fight", Vector3(0, 0, 0))
+	AudioManager.play_3d_sfx_for_all("unit_fight", enemy.global_position)
 	var current_attack = get_attack_damage()
 	damage(enemy.get_attack_damage())
 	enemy.damage(current_attack)
-	#if (enemy.card_id) == 
 
 
 	#If either attacker or defender survived actiave post attack and defend abilities
